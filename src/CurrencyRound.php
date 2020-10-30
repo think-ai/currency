dc40e04d429a780126d36b4a343912b1590db605
ThinkAi <thinkfullstack@gmail.com>
2020-10-30 13:09:56 +0530

    Rounding Logic Enhanced

--- src/CurrencyRound.php
+++ src/CurrencyRound.php
@@ -1,4 +1,5 @@
 <?php
+declare(strict_type=1);
 namespace ThinkAi;
 use InvalidArgumentException;
 
@@ -8,17 +9,26 @@ use InvalidArgumentException;
  */
 class CurrencyRound
 {
+    public const ROUND_UP = 1;
+
+    public const ROUND_DOWN = -1;
+
     /**
      * @param float $amount
      * @param float $multiply
-     * @throws InvalidArgumentException
+     * @param int $roundUpOrDown
      * @return float
      */
-    public static function round(float $amount, float $multiply = 0.01) : float
+    public static function round(float $amount, float $multiply = 0.01, int $roundUpOrDown = -1) : float
     {
         if ( $multiply <= 0 ) {
             throw new InvalidArgumentException("Argument 2 cannot be less than or Equal to Zero");
         }
-        return floor($amount/$multiply)*$multiply;
+        if ( $roundUpOrDown === self::ROUND_UP ) {
+            return ceil($amount/$multiply)*$multiply;
+        } else if ( $roundUpOrDown === self::ROUND_DOWN ) {
+            return floor($amount/$multiply)*$multiply;
+        }
+        throw new InvalidArgumentException("Argument 3 can be only 1 or -1");
     }
 }
